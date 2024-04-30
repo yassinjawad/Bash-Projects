d0be350ecc16a69eebd9a17ef7446ccf99ca00b1
“yassinjawad” <alabdulazizy@chc.edu>
2024-04-30 10:36:23 -0400

    04/30/2024

---
+++ assignment.sh
@@ -0,0 +1,16 @@
+#! /bin/bash
+
+# Computes and returns the total cost of all items 
+# starting with the first and continuing up 
+# to and including the item at the input number.
+get_total_cost () {
+    # You must update this return statement so that
+    # it returns the computed total cost.
+    items=( "Partridge in a Pear Tree" "Turtle Dove" "French Hen" "Calling Bird" "Gold Ring" 
+    "Geese a Laying" "Swans a Swimming" "Maides a Milking" "Ladies Dancing" "Lords A Leaping"
+    "Piper Piping" "Drummer Drumming")
+    
+    cost=("15" "27" "14" "9" "25" "5" "7" "18" "26")
+
+    return 1
+}
